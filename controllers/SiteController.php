acee7e2de491c16444e296ee5aebc167583377fe
Anastasia <anastasiatea@mail.ru>
2019-05-09 17:22:04 +0300

    3 практическая 2 вариант, вывод даты

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -61,7 +61,8 @@ class SiteController extends Controller
      */
     public function actionIndex()
     {
-        return $this->render('index');
+        $date = date("d:m:Y");
+        return $this->render('index', ['date' => $date]);
     }
 
     /**
